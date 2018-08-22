62b1681385d00ffd9de66ab32d67aa49442e84b1
Alexander Mladenov <alexander.r.mladenov@gmail.com>
2018-08-22 19:10:25 +0300

    Initial commit

---
+++ Metropolis/main.cpp
@@ -0,0 +1,21 @@
+# include <iostream>
+#include <embree3/rtcore.h>
+
+#include "xmmintrin.h"
+#include "pmmintrin.h"
+
+
+
+int main()
+{
+  _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+  _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
+  /* create new Embree device */
+  RTCDevice device = rtcNewDevice("verbose=1");
+
+  /* ddelete device again */
+  rtcReleaseDevice(device);
+  while (true) {}
+  return 0;
+
+}
